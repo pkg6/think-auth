0d4196cc48b29b00cfcaf07151bfc68beb4c5d6c
zhiqiang <zhiqiang2033@gmail.com>
2024-07-04 15:55:16 +0800

    1.1.x

--- tests/think.sh
+++ tests/think.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+./vendor/bin/php-cs-fixer fix
 
 php ./../../../think auth:test-init
-
 php ./../../../think auth:test-access
 php ./../../../think auth:test-auth
 php ./../../../think auth:test-jwt
